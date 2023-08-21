908e281970a79a89015d7fd44b800629dfea7873
Seo0H <emma05127@gmail.com>
2023-08-21 16:07:42 +0000

    Deploying to public from @ Seo0H/seo0h.github.io@70e483c8f3c43cfed194aeb45115480fa0fad4c1 🚀

--- _next/static/iJa8bkHBTHXUOvsgNiW0E/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-d581ea6cac43452b.js"],"/_error":["static/chunks/pages/_error-4afcb85b7c260fd3.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
