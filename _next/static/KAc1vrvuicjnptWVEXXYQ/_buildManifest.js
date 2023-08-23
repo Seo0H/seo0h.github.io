f020a0240c4ce1b78f0c6f6b65f3a535fb88307a
Seo0H <emma05127@gmail.com>
2023-08-23 08:39:46 +0000

    Deploying to public from @ Seo0H/seo0h.github.io@62e34279df6333599b1036a7801dfd5c9b8361b7 🚀

---
+++ _next/static/KAc1vrvuicjnptWVEXXYQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-04af10b2105dcb22.js"],"/_error":["static/chunks/pages/_error-11da4ebccdb36218.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
