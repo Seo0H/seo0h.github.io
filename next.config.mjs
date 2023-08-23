4560ff3fd354b8b8ded17b260e681a2e75f1b9be
SEO <108770949+Seo0H@users.noreply.github.com>
2023-08-24 00:23:42 +0900

    🚚 modify next config file to es6 module

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-const { withContentlayer } = require('next-contentlayer');
+import { withContentlayer } from 'next-contentlayer';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,5 +13,4 @@ const nextConfig = {
     styledComponents: true,
   },
 };
-
-module.exports = withContentlayer(nextConfig);
+export default withContentlayer(nextConfig);
