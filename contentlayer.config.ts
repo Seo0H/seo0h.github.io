dabb6a5861dd864d30bbcea8c7d0100373f6a0df
seo <emma05127@gmail.com>
2023-08-25 00:46:06 +0900

    ➕ add reading-time and contentlayer setting

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,5 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import readingTime from 'reading-time';
 
 export const Post = defineDocumentType(() => ({
   name: 'Post',
@@ -12,6 +13,14 @@ export const Post = defineDocumentType(() => ({
   },
   computedFields: {
     url: { type: 'string', resolve: (post) => `/${post._raw.flattenedPath}` },
+    readingTime: {
+      type: 'string',
+      resolve: (post) => Math.ceil(readingTime(post.body.raw).minutes),
+    },
+    wordCount: {
+      type: 'number',
+      resolve: (post) => post.body.raw.split(/\s+/gu).length,
+    },
   },
 }));
 
