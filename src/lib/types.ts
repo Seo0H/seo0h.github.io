0455fb753c190b6a2aa13d34b9639355ba98f4d6
seo <emma05127@gmail.com>
2023-08-29 17:26:43 +0900

    🏷️ BlogProps type 추가

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,7 @@
-import { Post as TPost } from 'contentlayer/generated';
+import { Post, Post as TPost } from 'contentlayer/generated';
 
 export type ReducedPost = Omit<Omit<Omit<TPost, 'body'>, '_raw'>, '_id'>;
+
+export interface BlogProps {
+  post: Post;
+}
