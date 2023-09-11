8ed53c701476e3804646d439f6122e21fd7feb0d
seo <emma05127@gmail.com>
2023-09-11 12:56:26 +0900

    📈 Add ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import Header from '@/components/common/header';
 import AppLayout from '@/components/layout/AppLayout';
 import GlobalStyles from '@/styles/globals';
@@ -7,6 +9,19 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      ></Script>
+      <Script id='google-analytics' strategy='afterInteractive'>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
       <GlobalStyles />
       <Header />
       <AppLayout>
