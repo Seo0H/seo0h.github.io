417e92de038a93dce489da05b1f718865c0995c4
seo <emma05127@gmail.com>
2023-09-11 17:24:23 +0900

    🔍 Add naver search meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,6 +41,7 @@ export default class MyDocument extends Document {
             crossOrigin=''
             href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/variable/pretendardvariable-dynamic-subset.css'
           />
+          <meta name='naver-site-verification' content='7ef5e520261e981fd6678836e9a97ea0b6ab1bd2' />
         </Head>
         <body>
           <Main />
