574054aad0f7ba2059b9f836747b5b63e2f6e47a
seo <emma05127@gmail.com>
2023-10-05 17:24:06 +0900

    Fix. contentslayer build error fix
    https://github.com/contentlayerdev/contentlayer/issues/129
    해당 이슈 참고하여 수정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-import { withContentlayer } from 'next-contentlayer';
+const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -15,4 +15,5 @@ const nextConfig = {
     },
   },
 };
-export default withContentlayer(nextConfig);
+
+module.exports = withContentlayer(nextConfig);
