947a27292919490f18f96c9c27be46f7dcc859bb
seo <emma05127@gmail.com>
2024-01-15 13:18:31 +0900

    🏷️ Update expand tag prop to Post type to make the type clearer

--- src/constants/blogDataset.ts
+++ src/constants/blogDataset.ts
@@ -1,12 +1,12 @@
 import { compareDesc } from 'date-fns';
 
-import { type Post, allPosts } from 'contentlayer/generated';
+import { type Post as ContentlayerPost, allPosts } from 'contentlayer/generated';
 
-import supabase from '@/api/client';
 import getTag from '@/lib/getTag';
-import { Tables } from '@/types/database.types';
 
-export const reducePost = ({ body: _, _raw, _id, ...post }: Post) => post;
+export const reducePost = ({ body: _, _raw, _id, ...post }: ContentlayerPost) => post;
+
+type Post = ContentlayerPost & { tag: string };
 
 export const cleanAllPost = allPosts
   .filter((post) => !post.draft)
@@ -16,7 +16,7 @@ export const cleanAllPost = allPosts
 
 export const allReducedPosts = cleanAllPost.map(reducePost);
 
-function enrichPostWithTag(post: Post) {
+function enrichPostWithTag(post: ContentlayerPost): Post {
   const { tag } = getTag(post);
   return {
     tag,
@@ -24,7 +24,7 @@ function enrichPostWithTag(post: Post) {
   };
 }
 
-function removeUUIDNewLine(post: Post): Post {
+function removeUUIDNewLine(post: ContentlayerPost) {
   return {
     ...post,
     uuid: post.uuid.replace(/\n|\r|\s*/g, ''),
@@ -37,72 +37,3 @@ export const AllTags = Array.from(
     return acc;
   }, new Set()),
 );
-
-type PostsIdAndTitle = {
-  id: string;
-  title: string;
-};
-
-/**
- * @description 서버 데이터 업데이트 판별 함수
- * @param serverPostsBeforeSorted
- * @param clientPostsBeforeSorted
- * @returns
- */
-export function calcUpdateNeeded(
-  serverPostsBeforeSorted: PostsIdAndTitle[],
-  clientPostsBeforeSorted: typeof cleanAllPost,
-) {
-  const serverPosts = [...serverPostsBeforeSorted].sort((a, b) => (a.id > b.id ? 1 : -1));
-
-  if (serverPosts.length === 0) return true;
-
-  const clientPosts = clientPostsBeforeSorted
-    .map((post) => ({ id: post.uuid, title: post.title }))
-    .sort((a, b) => (a.id > b.id ? 1 : -1));
-
-  const isClientAndServerPostCountSame = serverPosts?.length === clientPosts.length;
-  const isTitleAllMatch = clientPosts.every((clientPost, idx) => {
-    return serverPosts[idx] !== undefined && clientPost.title === serverPosts[idx].title;
-  });
-  const isUpdateNeeded = !isClientAndServerPostCountSame || isTitleAllMatch === false;
-
-  return isUpdateNeeded;
-}
-
-export async function updatePostsOnServer(
-  clientPosts: PostsIdAndTitle[],
-  serverPosts: PostsIdAndTitle[],
-) {
-  const insertPosts: Tables<'post'>[] = [];
-  const updatePosts: Tables<'post'>[] = [];
-
-  clientPosts.forEach((clientPost) => {
-    // 동기화 작업. 클라이언트의 포스트 기준 업데이트
-    const isUUIDExist = serverPosts.some((serverPost) => serverPost.id === clientPost.id);
-    const isTitleChanged =
-      isUUIDExist &&
-      serverPosts.some((serverPost) => serverPost.title === clientPost.title) === false;
-
-    if (!isUUIDExist) {
-      insertPosts.push(clientPost);
-    }
-
-    // id는 있지만 타이틀이 다른 경우
-    if (isTitleChanged) {
-      updatePosts.push(clientPost);
-    }
-  });
-
-  try {
-    if (insertPosts.length) {
-      await supabase.from('post').insert(insertPosts);
-    }
-
-    if (updatePosts.length) {
-      await supabase.from('post').upsert(updatePosts);
-    }
-  } catch (e) {
-    console.error(e);
-  }
-}
