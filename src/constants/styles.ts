330f1a2d460146e2c3a2045e8c9e9a37203914cc
seo <emma05127@gmail.com>
2024-01-22 13:02:41 +0900

    ♻️ Refactor css variable object structure
    - 전역 색상 스타일 객체(globalColor) 의 명도별 색상 표기 부분 구조 변경.
    - 기존에는 명도가 필요한 값을 array로 설정하고 각 요소의 인덱스 값을 이용해 CSS Variable를 생성하였으나 색상이 추가 될 시 Index 값도 같이 변해버리는 문제가 발생했기에 객체 구조 수정
    - globalColor객체 구조 수정에 따라 generateCssVar 유틸함수의 로직 수정

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,13 +1,17 @@
 /**
- * 색상 명도가 존재하는 경우, String[] 으로 표현
- * 명도가 연할수록 `(Array idx + 1) * 100` 으로 표현됨
- * ex) --gray--100 = '#E5E5E5'
+ * 전역 색상 변수 (css var)
  */
 
 export const globalColor = {
   mainColor: '#006fff',
   subColor: '#20399E',
-  gray: ['#E5E5E5', '#C2C2C2', '#727B87', '#4E5968', '#333D4B'],
+  gray: {
+    '100': '#E5E5E5',
+    '200': '#C2C2C2',
+    '300': '#727B87',
+    '400': '#4E5968',
+    '500': '#333D4B',
+  },
 } as const;
 
 export type GlobalColor = typeof globalColor;
--- src/utils/generateCssVar.ts
+++ src/utils/generateCssVar.ts
@@ -1,19 +1,22 @@
 import { type GlobalColor } from '@/constants/styles';
 
+/**
+ * @param globalColor 전역 스타일 CSS Variable이 선언되어있는 객체
+ * @description 전역 스타일 객체를 CSS Variable 형식의 String으로 변환하는 함수.
+ * 전역 스타일에 주입하기 위해 사용.
+ */
 export default function generateCssVar(globalColor: GlobalColor) {
-  return Object.entries(globalColor).reduce((prevCssVar, [key, val]) => {
-    if (isBrightness(val)) {
-      return `${prevCssVar}${val.reduce(getGrayCssVar, '')}`;
+  return Object.entries(globalColor).reduce((mergeCssVars, [key, val]) => {
+    // 명도로 표현되는 색상 판별
+    if (typeof val === 'object') {
+      return `${mergeCssVars}${Object.entries(val).reduce(getGrayCssVar, '')}`;
     }
 
-    return `${prevCssVar}--${key}:${val};`;
+    return `${mergeCssVars}--${key}:${val};`;
   }, '');
 }
 
-function getGrayCssVar(prev: string, color: string, idx: number) {
-  return `${prev}--gray-${idx + 1}00:${color};`;
-}
-
-function isBrightness(val: unknown): val is string[] {
-  return Array.isArray(val);
+// TODO: prefix값이 gray로 하드코딩 되어있는 부분 매개변수에서 받도록 수정
+function getGrayCssVar(prev: string, [brightness, color]: [string, string]) {
+  return `${prev}--gray-${brightness}:${color};`;
 }
