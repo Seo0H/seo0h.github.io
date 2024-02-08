828c09864c3d24605e1ac859df52f5c50b736f6c
seo <seo0h9@gmail.com>
2024-02-08 12:42:14 +0900

    Fix. client api 경로가 개발모드 또는 배포모드에 의존적이도록 설정

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// TODO: NODE_ENV 추가
-const isDev = true;
-const BACKEND_API_URL = isDev ? 'http://localhost:8080' : process.env.NEXT_PUBLIC_BACKEND_URL;
+const isDev = process.env.NODE_ENV === 'development';
+
+const BACKEND_API_URL = isDev ? 'https://localhost:8080' : process.env.NEXT_PUBLIC_BACKEND_ENDPOINT;
 
 const client = axios.create({
   baseURL: BACKEND_API_URL,
