594385dba323248175917335ca72acfb1982487a
seo <seo0h9@gmail.com>
2024-02-15 21:42:15 +0900

    ✨ Feat. 블로그 메인 화면에서 각 포스트의 실시간 조회수가 보이도록 구현

---
+++ src/lib/hooks/use-combined-post.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+
+import axios from 'axios';
+
+import GetAllPost from '@/api/post/get-all-post';
+import { type APIStatusType, isError, initialApiStatus } from '@/api/views/type';
+import { updateViewFromServerPost } from '@/components/blog-main/utils';
+
+import type { Post } from '@/types/post';
+
+/**
+ *  서버 데이터와 클라이언트 데이터를 합치는 커스텀 훅
+ * @returns {object} { posts : 서버 데이터 추가가 완료된 post 객체 , status : 서버 데이터 패칭 상태를 나타내는 변수 }
+ */
+const useCombinedPost = (initialPost: Post[]) => {
+  const [posts, setPosts] = useState(initialPost);
+  const [viewStatus, setViewApiStatus] = useState<APIStatusType>({
+    ...initialApiStatus,
+    isLoading: true,
+  });
+
+  useEffect(() => {
+    const CancelToken = axios.CancelToken;
+    const source = CancelToken.source();
+
+    const postViewFromServer = new GetAllPost(source);
+
+    postViewFromServer
+      .fetch()
+      .then((serverPosts) => {
+        if (isError(serverPosts) || serverPosts.length === 0) return;
+        setPosts((clientPosts) => updateViewFromServerPost(clientPosts, serverPosts));
+      })
+      .finally(() => {
+        setViewApiStatus(postViewFromServer.getStatus());
+      });
+
+    return () => {
+      source.cancel();
+    };
+  }, []);
+
+  return { posts, status: viewStatus };
+};
+
+export default useCombinedPost;
