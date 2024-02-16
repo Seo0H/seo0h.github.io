24019503044cb94c0abb68fc33d97f67b8be33b2
seo <seo0h9@gmail.com>
2024-02-16 15:02:15 +0900

    ♻️ Refactor. TagFilter 컴포넌트 로직 분리
    - TagFilter컴포너트와 PostList가 유기적으로 작동하기에 이 둘이 공유하는 데이터에 한해 Context 적용

--- src/components/blog-main/index.tsx
+++ src/components/blog-main/index.tsx
@@ -1,22 +1,34 @@
+import { useState } from 'react';
+
 import { PageSEO } from '@/components/SEO';
+import PostsContext from '@/components/blog-main/context';
 import Greeting from '@/components/blog-main/greeting';
 import PostList from '@/components/blog-main/post-list';
+import TagFilter from '@/components/blog-main/tag-filter';
 import * as Layout from '@/components/layout';
 import { Post } from '@/types/post';
+
+import type { APIStatusType } from '@/api/views/type';
 
 export type BlogMainProps = {
   posts: Post[];
   tags: string[];
 };
 
-const BlogMain = (props: BlogMainProps) => {
+const BlogMain = (props: BlogMainProps & { apiStatus: APIStatusType }) => {
+  const { posts: allPosts, apiStatus } = props;
+  const [displayPosts, setDisplayPosts] = useState(allPosts);
+
   return (
     <>
       <PageSEO />
 
       <Layout.Box padding='0 10px' margin='0 0 100px 0' width='92%'>
         <Greeting />
-        <PostList {...props} />
+        <PostsContext.Provider value={{ allPosts, displayPosts, setDisplayPosts, apiStatus }}>
+          <TagFilter tags={props.tags} />
+          <PostList />
+        </PostsContext.Provider>
       </Layout.Box>
     </>
   );
--- src/components/blog-main/post-list.tsx
+++ src/components/blog-main/post-list.tsx
@@ -1,57 +1,36 @@
 import { AnimatePresence, motion } from 'framer-motion';
 
-import TagFilter from '@/components/blog-main/tag-fliter';
+import { usePostsContext } from '@/components/blog-main/context';
 import * as Layout from '@/components/layout';
 import PostPreview from '@/components/post-preview';
 import PostPreviewSkeleton from '@/components/post-preview/skeleton';
-import { fadeIn, staggerHalf } from '@/lib/animations';
-import useCombinedPost from '@/lib/hooks/use-combined-post';
-import useFilteredPost from '@/lib/hooks/use-filtered-post';
+import { staggerHalf } from '@/lib/animations';
 
-import type { BlogMainProps } from '@/components/blog-main';
-
-const PostList = ({ posts: initialPost, tags }: BlogMainProps) => {
-  const { posts, status: postStatus } = useCombinedPost(initialPost);
-  const { filteredPosts, selectedTag, setTag } = useFilteredPost(posts, 'ALL');
-
-  const handleTagFilter = (tagName: string | 'ALL') => {
-    setTag(tagName);
-  };
+const PostList = () => {
+  const { displayPosts, apiStatus: postApiStatus } = usePostsContext();
 
   return (
-    <>
-      <motion.section variants={fadeIn} initial='initial' animate='animate'>
-        <Layout.HStack margin='0 0 15px 0' gap='10px'>
-          <TagFilter onSelect={handleTagFilter} tags={tags} selectedTag={selectedTag} />
-        </Layout.HStack>
-      </motion.section>
-
-      <motion.section variants={staggerHalf} initial='initial' animate='animate'>
-        <motion.div variants={staggerHalf}>
-          <Layout.VStack gap='20px'>
-            <AnimatePresence>
-              {(postStatus.isLoading || postStatus.isAbort) && (
-                <motion.div
-                  initial={{ opacity: 0.8 }}
-                  animate={{ opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                >
-                  <Layout.VStack width='100%' gap='20px'>
-                    <PostPreviewSkeleton />
-                    <PostPreviewSkeleton />
-                    <PostPreviewSkeleton />
-                  </Layout.VStack>
-                </motion.div>
-              )}
-              {(postStatus.isSuccess || postStatus.isError) &&
-                filteredPosts.map((post) => (
-                  <PostPreview key={crypto.randomUUID()} post={post} viewStatus={postStatus} />
-                ))}
-            </AnimatePresence>
-          </Layout.VStack>
-        </motion.div>
-      </motion.section>
-    </>
+    <motion.section variants={staggerHalf} initial='initial' animate='animate'>
+      <motion.div variants={staggerHalf}>
+        <Layout.VStack gap='20px'>
+          <AnimatePresence>
+            {(postApiStatus.isLoading || postApiStatus.isAbort) && (
+              <motion.div initial={{ opacity: 0.8 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+                <Layout.VStack width='100%' gap='20px'>
+                  <PostPreviewSkeleton />
+                  <PostPreviewSkeleton />
+                  <PostPreviewSkeleton />
+                </Layout.VStack>
+              </motion.div>
+            )}
+            {(postApiStatus.isSuccess || postApiStatus.isError) &&
+              displayPosts.map((post) => (
+                <PostPreview key={crypto.randomUUID()} post={post} viewStatus={postApiStatus} />
+              ))}
+          </AnimatePresence>
+        </Layout.VStack>
+      </motion.div>
+    </motion.section>
   );
 };
 
